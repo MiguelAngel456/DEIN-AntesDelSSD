69a7cc3653b795eed5a7e68bd1832e1eaa18a8b7
dm2 <dm2@dm2-07>
2022-09-26 10:02:08 +0200

    lunes26sep

--- TANDA1/src/module-info.java
+++ TANDA1/src/module-info.java
@@ -7,5 +7,6 @@ module TANDA1 {
 	opens ejercicioC to javafx.graphics, javafx.fxml;
 	opens ejercicioD to javafx.graphics, javafx.fxml;
 	opens ejercicioE to javafx.graphics, javafx.fxml;
+	opens ejercicioF to javafx.graphics, javafx.fxml;
 	opens model to javafx.graphics, javafx.fxml, javafx.base;
 }
